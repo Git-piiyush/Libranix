db2f39fd4e1c8ea9fd7d1b88131090a9fb849565
Piyush <piyushchoudhary6681@gmail.com>
2021-05-26 00:31:16 +0530

    dotenv to dependencies

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').load()
-// }
-const dotenv = require('dotenv');
-dotenv.config();
+
+require('dotenv').config();
+
 
 const express = require('express')
 const app = express()
